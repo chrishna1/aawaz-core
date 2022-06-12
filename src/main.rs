ec63d9dcf11bbd88b7594420dba621ad3c2a6bbd
chrishna1 <adjnct@gmail.com>
2022-06-12 18:56:29 +0530

    Allow request from other origins

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use actix_cors::Cors;
 use actix_web::{App as ActixWebApp, HttpServer};
 
 use aawaz::api_routes;
@@ -8,6 +9,10 @@ pub async fn main() -> std::io::Result<()> {
 
     HttpServer::new(|| {
         ActixWebApp::new()
+            .wrap(
+                // TODO - restrict it to only supported methods, headers, etc.
+                Cors::permissive()
+            )
             .configure(|cfg| api_routes::config(cfg, "/api/v1"))
     })
     .bind(("127.0.0.1", 8080))?
